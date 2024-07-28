71f65420a5e83b9331f7c1a84acf3d6aa5ff5182
Brian <overthebrain@outlook.com>
2024-07-28 04:08:03 -0700

    HW1 done

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,11 +9,19 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        assertEquals(1, arb.dequeue());
+        assertTrue(arb.fillCount() == 2);
+        arb.dequeue();
+        arb.dequeue();
+        assertTrue(arb.isEmpty());
     }
 
     /** Calls tests for ArrayRingBuffer. */
     public static void main(String[] args) {
         jh61b.junit.textui.runClasses(TestArrayRingBuffer.class);
     }
-} 
+}
